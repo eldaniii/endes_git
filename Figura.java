f1129203225230b8cf4f5e8d5f65e80683143427
eldanii <dvilmar309@g.educaand.es>
2024-02-23 12:38:30 +0100

    Añadir comentarios a la clase Figura

--- Figura.java
+++ Figura.java
@@ -1,4 +1,19 @@
+/**
+ Representa estructura básica de figura geométricas.
+ Las subclases deben implementar los métodos para calcular área y perímetro.
+
+ @author eldanii
+*/
 public abstract class Figura {
+
+	/**
+	Retorna el área de la figura
+	@return Área como double
+	*/
 	public abstract double area();
-public abstract double perimetro();
+	/**
+	Retorna el perímetro de la figura
+	@return Perímetro como double
+	*/
+	public abstract double perimetro();
 }
